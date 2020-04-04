52e0b39f33b3a30e382ad143ffacbdac46d88dd4
Lera Bilshchuk <leratheone@gmail.com>
2020-04-04 09:24:25 +0300

    Импортирует компоненты в точку входа

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+import {createBoardTemplate} from "./components/board.js";
+import {createFilterTemplate} from "./components/filter.js";
+import {createLoadMoreButtonTemplate } from "./components/load-more-button.js";
+import {createSiteMenuTemplate} from "./components/site-menu.js";
+import {createSortingTemplate} from "./components/sorting.js";
+import {createTaskEditTemplate} from "./components/task-edit.js";
+import {createTaskTemplate} from "./components/task.js";
+
 const TASK_COUNT = 3;
 
 const render = (container, template, place) => {
@@ -11,13 +19,13 @@ render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
 render(siteMainElement, createFilterTemplate(), `beforeend`);
 render(siteMainElement, createBoardTemplate(), `beforeend`);
 
-const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 const boardElement = siteMainElement.querySelector(`.board`);
+const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 
+render(boardElement, createSortingTemplate(), `afterbegin`);
 render(taskListElement, createTaskEditTemplate(), `beforeend`);
 
 for (let i = 0; i < TASK_COUNT; i++) {
   render(taskListElement, createTaskTemplate(), `beforeend`);
 }
-
 render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
